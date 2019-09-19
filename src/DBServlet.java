ee5728e27c468816770fb112b7e66a3b2d509884
Ottermonk <ottermonk@gmail.com>
2019-09-18 19:17:29 -0500

    Instructor Stacy homework complete

--- src/DBServlet.java
+++ src/DBServlet.java
@@ -6,37 +6,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
 
-@WebServlet(name = "SecondServlet", urlPatterns = "/test")
-public class SecondServlet extends HttpServlet {
+@WebServlet(name = "DBServlet", urlPatterns = "/search")
+public class DBServlet extends HttpServlet {
     private final String DRIVER_NAME = "jdbc:derby:";
     private final String DATABASE_PATH = "/WEB-INF/lib/DuehrDB";
     private final String USERNAME = "Ottermonk";
     private final String PASSWORD = "Pokemon1";
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String p2 = request.getParameter("param2");
-        String message = "<html><body>You said: " + p2 + "</body></html>";
-        response.getWriter().print(message);
+    // Comment
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String p1 = request.getParameter("param1");
-        String message = "<html><body>You said: " + p1 + "</body></html>";
-        response.getWriter().print(message);
+        // Declare outside the try/catch so the variables are in scope in the finally block
+        String results = request.getParameter("results");
 
-        //String msg = "<html><body>Hello World</body></html>";
-        //response.getWriter().print(msg);
         Connection conn = null;
-        PreparedStatement pstmt = null;
+        PreparedStatement stmt = null;
         ResultSet rset = null;
 
         try {
-            String searchTerm = request.getParameter("-----");
-
             // Load the driver
-            Class.forName("org.aache.derby.jdbc.EmbeddedDriver");
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
             // Find the absolute path of the database folder
             String absPath = getServletContext().getRealPath(DATABASE_PATH);
@@ -45,18 +38,17 @@ public class SecondServlet extends HttpServlet {
             sql.append("from CUSTOMER, CUSTOMERORDER, PRODUCT, PRODUCTORDER ");
             sql.append("where customerOrder.customer_id = customer.customer_id and ");
             sql.append("productOrder.product_id = product.product_id and ");
-            sql.append("customerOrder.order_id = ProductOrder.order_id ");
+            sql.append("customerOrder.order_id = ProductOrder.order_id and ");
+            sql.append("customerOrder.order_Id = ? ");
             sql.append("order by customer_nm, customerOrder.order_id "); // Don't end SQL with semicolon!
-
 
             // Create a connection
             conn = DriverManager.getConnection(DRIVER_NAME + absPath, USERNAME, PASSWORD);
             // Create a statement to execute SQL
-            pstmt = conn.prepareStatement(sql.toString());
-            // Fill the prepared statement params
-            pstmt.setString(1, searchTerm);
+            stmt = conn.prepareStatement(sql.toString());
+            stmt.setInt(1, Integer.parseInt(results));
             // Execute a SELECT query and get a result set
-            rset = pstmt.executeQuery();
+            rset = stmt.executeQuery();
 
             // Create a StringBuilder for ease of appending strings
             StringBuilder output = new StringBuilder();
@@ -92,9 +84,9 @@ public class SecondServlet extends HttpServlet {
                     e.printStackTrace();
                 }
             }
-            if (pstmt != null) {
+            if (stmt != null) {
                 try {
-                    pstmt.close();
+                    stmt.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -109,4 +101,3 @@ public class SecondServlet extends HttpServlet {
         }
     }
 }
-//commit
